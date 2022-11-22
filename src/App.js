300ae693d0d58597944d0ab54118ea6856a19f9d
Guillem <liammartinez@Liams-MBP.home>
2022-11-22 11:51:07 +0100

    new commit guillem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import './global.scss';
+import Home from './pages/Home/Home';
+import Search from './pages/Search/Search';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import { 
   BrowserRouter as Router, 
   Routes, 
-  Route } from 'react-router-dom';
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/search' element={<Search />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
