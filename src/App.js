e5a37d192e8cb1b1e1c8026e77a750ce497800f0
Guillem <guillemmj33@gmail.com>
2022-11-25 11:30:36 +0100

    new commit guillem

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+            <Route path='/' element={<Home />}></Route>
             <Route path='/login' element={<Login />}></Route>
-            <Route path='/home' element={<Home />}></Route>
             <Route path='/search' element={<Search />}></Route>
         </Routes>
       </Router>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { FilterBtnPopular, FilterBtnMedium, FilterBtnWorst } from '../../components/Buttons/Filter/FilterBtn';
+import { DevicesBtnMobile, DevicesBtnTablet, DevicesBtnDesktop } from '../../components/Buttons/Devices/DevicesBtn';
 
 const Home = () => {
   return (
     <main>
-      <section>
-        
-      </section>
+      <div className="filterBtn">
+        <FilterBtnPopular><i class="fa-regular fa-face-laugh"></i>Populares</FilterBtnPopular>
+        <FilterBtnMedium><i class="fa-regular fa-face-meh"></i>Intermedios</FilterBtnMedium>
+        <FilterBtnWorst><i class="fa-regular fa-face-frown"></i>Peor Valorados</FilterBtnWorst>
+      </div>
+      <div className="devicesBtn">
+        <DevicesBtnMobile><i class="fa-solid fa-mobile-screen-button"></i>Versión móvil</DevicesBtnMobile>
+        <DevicesBtnTablet><i class="fa-solid fa-tablet-screen-button"></i>Versión tablet</DevicesBtnTablet>
+        <DevicesBtnDesktop><i class="fa-solid fa-desktop"></i>Versión escritorio</DevicesBtnDesktop> 
+      </div>
     </main>
   )
 };
