0e7081495af8fe12cee6960a453d3d8affd1e1da
Mariovalcen <mariovalcen@gmail.com>
2022-12-04 21:58:03 +0100

    incoming change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-import './App.css';
-import './global.scss';
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
-import Info from './pages/Info/Info';
-import Navbar from './components/Navbar/Navbar.jsx';
-import Trending from './components/Trending/Trending';
-import MediasDesktop from './components/Medias/MediasDesktop';
-=======
 import "./App.css";
 import "./global.scss";
 import Home from "./pages/Home/Home";
 import Search from "./pages/Search/Search"
 import Main from '../src/components/Main/Main.jsx';
 
->>>>>>> mamadouDev
 
 
 
@@ -24,20 +13,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Router>
-        <MediasDesktop></MediasDesktop>
-        <Navbar/>
-        <Trending/>
-
-        <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/search' element={<Search />}></Route>
-            <Route path='/info:id' element={<Info />}></Route>
-        </Routes>
-        
-      </Router>
-=======
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
@@ -47,7 +22,6 @@ function App() {
           <Route path="/app" element={<Main />}></Route>
         </Routes>
       </BrowserRouter>
->>>>>>> mamadouDev
     </div>
   );
 }
